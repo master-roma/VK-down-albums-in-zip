279f840dc67ee02fd79998c2681c7877802d75f5
Master Roma <57301120+master-roma@users.noreply.github.com>
2025-10-26 00:52:08 +0300

    Fix formatting and update metadata in userscript

--- VK_down_albums_in_zip.js
+++ VK_down_albums_in_zip.js
@@ -1,16 +1,18 @@
 // ==UserScript==
-// @name              VK: down albums in .zip
-// @version           2025.06.15.beta
-// @author            Master Roma
-// @description       Adds abblity to dowhload VK album in .zip
-// @description:ru    Добавляет возможность скачивать VK альбомы в .zip
-// @icon              data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSIjODI4YTk5IiBkPSJtIDEwLDYgaCA0IHYgNiBoIDMgbCAtNSw2IC01LC02IGggMyB6IiBwYWludC1vcmRlcj0ibWFya2VycyBzdHJva2UgZmlsbCIvPjwvc3ZnPg==
-// @include           *vk.com/*
-// @include           *vk.ru/*
-// @match             *vk.com/*
-// @match             *vk.ru/*
+// @name				VK: down albums in .zip
+// @version				2025.06.15.beta
+// @author				Master Roma
+// @description			Adds abblity to dowhload VK album in .zip
+// @description:ru		Добавляет возможность скачивать VK альбомы в .zip
+// @icon				data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSIjODI4YTk5IiBkPSJtIDEwLDYgaCA0IHYgNiBoIDMgbCAtNSw2IC01LC02IGggMyB6IiBwYWludC1vcmRlcj0ibWFya2VycyBzdHJva2UgZmlsbCIvPjwvc3ZnPg==
+// @include				*vk.com/*
+// @include				*vk.ru/*
+// @match				*vk.com/*
+// @match				*vk.ru/*
 // @noframes
-// @run-at            document-end
+// @run-at            	document-end
+// @updateURL			https://github.com/master-roma/VK-down-albums-in-zip/blob/main/VK_down_albums_in_zip.js
+// @downloadURL			https://github.com/master-roma/VK-down-albums-in-zip/blob/main/VK_down_albums_in_zip.js
 // ==/UserScript==
 //____________________________________________________________________________________________________________________________________
 
