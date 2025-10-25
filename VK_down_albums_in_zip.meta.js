b91c6e0f8bdb5d161cfc5595cfdafdf09601de81
Master Roma <57301120+master-roma@users.noreply.github.com>
2025-10-26 01:01:48 +0300

    Add metadata for VK album download userscript

---
+++ VK_down_albums_in_zip.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name				VK: down albums in .zip
+// @name:ru				VK: качалка альбомов в .zip
+// @version				2025.10.26.beta
+// @author				Master Roma
+// @description			Adds abblity to dowhload VK album in .zip
+// @description:ru		Добавляет возможность скачивать VK альбомы в .zip
+// @icon				data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSIjODI4YTk5IiBkPSJtIDEwLDYgaCA0IHYgNiBoIDMgbCAtNSw2IC01LC02IGggMyB6IiBwYWludC1vcmRlcj0ibWFya2VycyBzdHJva2UgZmlsbCIvPjwvc3ZnPg==
+// @include				*vk.com/*
+// @include				*vk.ru/*
+// @match				*vk.com/*
+// @match				*vk.ru/*
+// @noframes
+// @run-at            	document-end
+// @updateURL			https://github.com/master-roma/VK-down-albums-in-zip/blob/main/VK_down_albums_in_zip.js
+// @downloadURL			https://github.com/master-roma/VK-down-albums-in-zip/blob/main/VK_down_albums_in_zip.meta.js
+// ==/UserScript==
